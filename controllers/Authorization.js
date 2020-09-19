b1a36a58f7485c4cb1dd152914c4a8e27be74f9e
Kmilka <do.kseniya@gmail.com>
2020-09-19 17:30:57 +0300

    test auth error

--- controllers/Authorization.js
+++ controllers/Authorization.js
@@ -3,16 +3,16 @@ const requireAuth = (redisClient) =>  (req, res, next) => {
     if (authorization.includes('Bearer')) {
         const token = authorization.slice(7,authorization.length);
         if (!token) {
-            return res.status(401).json('unauthorized')
+            return res.status(401).json('there is no token')
         }
         return redisClient.get(token, (err, reply) => {
             if (err || !reply) {
-                return res.status(401).json('unathorized')
+                return res.status(401).json('err or no reply from redis')
             }
             return next()
         })
     } else {
-        return res.status(401).json('unathorized');
+        return res.status(401).json('no bearer');
     }
 }
 
